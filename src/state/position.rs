f391a22370dbf959b69b5df5229edd630872d549
Philipp Reiter <philipp.reiter@freiheit.com>
2023-11-28 19:40:53 +0100

    fix selection backwared

--- src/state/position.rs
+++ src/state/position.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct Position {
     pub line: usize,
@@ -8,5 +10,14 @@ impl Position {
     #[must_use]
     pub fn new(line: usize, column: usize) -> Self {
         Self { line, column }
+    }
+}
+impl PartialOrd for Position {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.line.cmp(&other.line) {
+            std::cmp::Ordering::Less => Some(std::cmp::Ordering::Less),
+            std::cmp::Ordering::Greater => Some(std::cmp::Ordering::Greater),
+            std::cmp::Ordering::Equal => self.column.partial_cmp(&other.column),
+        }
     }
 }
