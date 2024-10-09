10bb536396e0c24927e20787076b2086082a789c
Philipp Reiter <p.reiter93@gmail.con>
2024-10-09 22:52:44 +0200

    Update demo

--- examples/app/main.rs
+++ examples/app/main.rs
@@ -27,7 +27,7 @@ Navigate right (l), left (h), up (k) and down (j), using vim motions.
 
 Traverse words forward (w) and backward (b).
 
-Select text (v), including selection between \"delimiters\" (ciw).
+Select text (v), including selection between \"quotes\" (ciw).
 
 Copy and paste text: 
 
